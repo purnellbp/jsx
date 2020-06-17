bef4239964f97a5e4f191a5cbbf90ed34b90fd2e
Brian Purnell <purnellbp@gmail.com>
2020-06-17 15:40:29 -0500

    modified jsx styling for react app

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-    return <div>Hi There!</div>;
+    return (
+        <div>
+            <label className="label" for="name">Enter name:</label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+        </div>
+    );
 }
 
 // Take the react and show it on the screen
