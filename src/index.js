9f949869d89fda46b89444d0786776960c675f33
Brian Purnell <purnellbp@gmail.com>
2020-06-18 10:45:07 -0500

    react boiler

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
+    const getButtonText = "CLICK ME!";
     return (
         <div>
-            <label className="label" for="name">Enter name:</label>
+            <label className="label" htmlFor="name">Enter name:</label>
             <input id="name" type="text"/>
-            <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{getButtonText}</button>
         </div>
     );
 }
